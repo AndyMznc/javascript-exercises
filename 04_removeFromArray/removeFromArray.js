781f446cad50052299a3f639d7a2b8959aeec485
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-22 22:40:52 +0200

    Complétion - Exercice 4
    Complétion de l'exercice 4 sur le TDD.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (myArray, ...toRemove) {
+  return myArray.filter((element) => !toRemove.includes(element));
 };
+removeFromArray([1, 2, 3, 4], 3, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
