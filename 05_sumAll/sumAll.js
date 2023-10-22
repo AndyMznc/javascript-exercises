edd1543036425ad003c950d4fbf9c17067d40e79
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-22 23:58:45 +0200

    Complétion - Exercice 5
    Complétion de l'exercice 5 sur le TDD.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let total = 0;
 
+  function isNumber(value) {
+    return typeof value === "number";
+  }
+
+  if (!isNumber(num1) || !isNumber(num2)) {
+    return "ERROR";
+  }
+
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  for (let i = num1; i <= num2; i++) {
+    total += i;
+  }
+
+  for (let i = num2; i <= num1; i++) {
+    total += i;
+  }
+
+  return total;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
