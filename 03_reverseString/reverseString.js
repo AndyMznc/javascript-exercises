cc68d81990c5ac01a8850a2a1dcbf2dec825253e
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-22 19:19:11 +0200

    Complétion - Exercice 3
    Complétion de l'exercice 3 sur le TDD.
    
    Solution horrible de ma part en me compliquant la vie. J'aurais pu utiliser la méthode
    reverse() pour inverser l'ordre de mon tableau.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,25 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  //  [x] TODO: INITIALISER ma variable de mon mot inversé
+  let reversedWord = "";
 
+  // [x] TODO: INITIALISER un nouveau tableau qui stockera mon mot inversé
+  let letterWord = [];
+
+  // POUR CHAQUE lettres de mon mot
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    letterWord.push(letter);
+  }
+
+  // [x] TODO: AJOUTER le dernier élément dans mon nouveau tableau
+  while (letterWord.length > 0) {
+    let lastLetter = letterWord.slice(-1);
+    reversedWord += lastLetter;
+    letterWord.pop();
+  }
+
+  // [x] TODO: RETOURNER mon résultat.
+  return reversedWord;
 };
 
 // Do not edit below this line
