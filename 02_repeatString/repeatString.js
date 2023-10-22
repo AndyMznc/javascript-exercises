1ca1d2c0895e72a652629518e7b9768e6a4c0079
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-22 16:14:21 +0200

    Complétion - Exercice 2
    Complétion de l'exercice 2 sur le TDD.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let sentence = "";
 
+  if (num < 0) {
+    return (sentence = "ERROR");
+  }
+
+  while (num > 0) {
+    sentence += string;
+
+    num--;
+  }
+
+  return sentence;
 };
 
 // Do not edit below this line
