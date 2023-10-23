1b6ee497880e6c76e14af74b6bddc2b5803a9ae4
Andy <115222358+AndyMznc@users.noreply.github.com>
2023-10-23 22:11:31 +0200

    Complétion - Exercice 7
    Complétion de l'exercice 7 sur le TDD.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9.
+
+  const FORMULA = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(FORMULA * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+
+  const FORMULA = (celsius * 9) / 5 + 32;
+  return Math.round(FORMULA * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
